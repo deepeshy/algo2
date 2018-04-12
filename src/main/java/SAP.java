1a7ce859b62e8bc90226cb5830fbd388ff381bad
deepeshy <programmer.dy@gmail.com>
2018-04-13 00:53:11 +0530

    fixed import to comply with the standard, IDE autoconverted import to have *, fails checkstyle

--- src/main/java/SAP.java
+++ src/main/java/SAP.java
@@ -1,4 +1,8 @@
-import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
 
 import java.util.Iterator;
 
